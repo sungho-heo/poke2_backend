4206cd89376e8b3f433c346390d4a1c1d996a9f0
Sungho <hurgj123kr@gmail.com>
2024-08-01 20:36:54 +0900

    fav routee => router update..

--- src/routes/fav.ts
+++ src/routes/fav.ts
@@ -1,11 +1,11 @@
-import Route from "express";
+import { Router } from "express";
 import { getFav, addFav, removeFav } from "../controllers/favController";
 import authMiddleware from "../middleware/authMiddleware";
 
-const router = Route();
+const router = Router();
 
 router.get("/", authMiddleware, getFav);
-router.post("/add/", authMiddleware, addFav);
+router.post("/add", authMiddleware, addFav);
 router.delete("/remove/:pokemonName", authMiddleware, removeFav);
 
 export default router;
