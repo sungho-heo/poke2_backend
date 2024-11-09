6912e44ca93b5bda7b4a7a6fd48ebecf4bcbf256
Sungho <hurgj123kr@gmail.com>
2024-11-09 20:17:26 +0900

    deploy 준비

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+};
+exports.default = config;
